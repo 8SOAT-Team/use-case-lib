79a951ca70c0e110d82f5419e3f9b5456a8e706d
Felipe Bergmann <felipe.bergmann.f@gmail.com>
2025-01-06 22:14:02 -0300

    feat: first version

---
+++ src/CleanArch.UseCase/IUseCase.cs
@@ -0,0 +1,15 @@
+﻿using CleanArch.UseCase.Faults;
+using CleanArch.UseCase.Options;
+
+namespace CleanArch.UseCase;
+
+public interface IUseCase
+{
+    public bool IsFailure { get; }
+    public IReadOnlyCollection<UseCaseError> GetErrors();
+}
+
+public interface IUseCase<TCommand, TOut> : IUseCase where TOut : class
+{
+    public Task<Any<TOut>> ResolveAsync(TCommand command);
+}
---
+++ src/UseCase.Tests/Unit/Options/AnyTest.cs
@@ -0,0 +1,35 @@
+﻿using CleanArch.UseCase.Options;
+using FluentAssertions;
+using UseCase.Tests.MockStudio;
+
+namespace UseCase.Tests.Unit.Options;
+
+public sealed class AnyTest
+{
+    [Fact]
+    public void Some_Given_InputValue_Should_BeSomeWithInputValueValue()
+    {
+        // Arrange
+        var input = FakeIt.Faker.Random.Word();
+
+        // Act
+        var sut = Any<string>.Some(input);
+
+        // Assert
+        sut.Should().BeOfType<Some<string>>();
+        sut.Value.Should().Be(input);
+        sut.HasValue.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Empty_Given_InputValueIsEmpty_Should_BeEmptyAndHasValueFalse()
+    {
+        // Arrange
+        // Act
+        var sut = Any<string>.Empty;
+
+        // Assert
+        sut.Should().BeOfType<Empty<string>>();
+        sut.HasValue.Should().BeFalse();
+    }
+}
