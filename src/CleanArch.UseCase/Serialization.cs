890cc1218b4f896b3ccd9962a3644efe4f901d05
Felipe Bergmann <felipe.bergmann.f@gmail.com>
2025-02-09 00:10:04 -0300

    fix: json serialization cycles

---
+++ src/CleanArch.UseCase/Serialization.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CleanArch.UseCase;
+
+internal static class Serialization
+{
+    internal static readonly JsonSerializerOptions JsonSerializerOptions = new()
+    {
+        WriteIndented = true,
+        ReferenceHandler = ReferenceHandler.Preserve,
+        Converters = { new JsonStringEnumConverter() }
+    };
+}
